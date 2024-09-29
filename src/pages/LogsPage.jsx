14eed1690c4f2a1ae5df590daa2a26a44d82545f
Mahitab Elbehery <mahitab.elbehery@gmail.com>
2024-09-29 14:35:09 +0300

    fixing path again

--- src/pages/LogsPage.jsx
+++ src/pages/LogsPage.jsx
@@ -3,7 +3,7 @@ import { Sidebar } from '@/components/ui/sidebar';
 import { Button } from '../components/ui/button'; 
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'; 
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '../components/ui/Tabs'; 
-import { Card, CardHeader, CardTitle, CardContent } from '../components/ui/card'; 
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'; 
 
 const LogsPage = () => {
   const menuItems = ['Home', 'Logging', 'Trainers and Users', 'Settings'];
